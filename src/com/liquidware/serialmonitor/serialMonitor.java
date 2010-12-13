2438100e766bc011beb04e8ab99f7ed82cb9ceb0
Chris <chris.ladden@liquidware.com>
2010-12-13 06:54:39 -0500

    Added hacked GPS interface routines to communicate with the serial daemon.

--- src/com/liquidware/serialmonitor/serialMonitor.java
+++ src/com/liquidware/serialmonitor/serialMonitor.java
@@ -1,51 +1,105 @@
 package com.liquidware.serialmonitor;
 
+import android.util.*;
+import android.content.Context;
+import android.location.*;
+import android.location.GpsStatus.*;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-//import android.widget.TextView;
 
 public class serialMonitor extends Activity {
+	private static final String TAG = "SerialMonitor";
+	TextView tv;
+	
 	/** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState) {	
+	public void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
-      
-	setContentView(R.layout.main);
-    final TextView tv = (TextView) findViewById(R.id.message);
-    
-	//Send button
-	final Button button = (Button) findViewById(R.id.ok);
-	button.setOnClickListener(new View.OnClickListener() {
-	public void onClick(View v) {
+		setContentView(R.layout.main);
 		
-		// Perform action on click
-		EditText et = (EditText) findViewById(R.id.entry);
-		//et.setText("");
+		//Text View
+	    tv = (TextView) findViewById(R.id.message);
+	    tv.setLines(14);
 		
-		tv.setText("> " + et.getText().toString() + "\n"); //tv.getText() + "234343 " + "\n34424" + "\n3434334");
-		tv.setLines(14);
+	    //Editext field
+		final EditText et = (EditText) findViewById(R.id.entry);
 		
-		//tv.setVerticalScrollBarEnabled(true);
-		//tv.setScrollBarStyle(1);
-		//tv.refreshDrawableState();
-		//et.setText("> " + et.getText().toString() + "\n");	
-	 }
-	});
+		//Clear Button
+		final Button cButton = (Button) findViewById(R.id.xxl);
+		
+		add_text("About to create lm");
+		final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		add_text("Done creating lm");
+		
+		//Send Button
+		final Button sButton = (Button) findViewById(R.id.ok);
+		sButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				lm.SerialPrint("Hi Ardy!");
+				//et.setText("");
+			}
+		});
+		
+		//Begin button
+		final Button bButton = (Button) findViewById(R.id.begin);
+		bButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				
+				add_text("About to create ll");
+				final LocationListener ll = new LocationListener(){
+				       //sample listener...
+					public void onLocationChanged(Location location) { }
+					public void onStatusChanged(String provider, int status, Bundle extras) { }
+					public void onProviderEnabled(String provider) { }
+					public void onProviderDisabled(String provider) { }
+				};
+				
+
+				
+				add_text("Creating a SerialMsgListener");
+				final GpsStatus.SerialMsgListener nl = new GpsStatus.SerialMsgListener() {
+				   @Override
+				   public void onSerialMsgReceived(String sMsg) {
+				      /*
+				       * Broadcast a message..
+				       */
+					   add_text(sMsg);
+				   }
+				};
+				
+				lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 50, 0, ll);
+				lm.addSerialMsgListener(nl);
+			 }
+		
+
+		});
 	
-	//Clear Button
-	final Button xxl = (Button) findViewById(R.id.xxl);
-	xxl.setOnClickListener(new View.OnClickListener() {
+	cButton.setOnClickListener(new View.OnClickListener() {
 	public void onClick(View v) {
-		
-		// Perform action on click
-		EditText et = (EditText) findViewById(R.id.entry);
 		et.setText("");
 		tv.setText("");
 	  }
 	});
+	
+
   }
+	
+	/**
+	 * Utility
+	 */
+	int line_count = 0;
+	private void add_text(String s) {
+		if (line_count > 10) {
+			tv.setText("");
+			line_count = 0;
+		}
+		tv.setText(tv.getText() + "\n>" + s);
+		line_count++;
+	}	
+	
 }
